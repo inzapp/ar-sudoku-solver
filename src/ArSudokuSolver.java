14e72f60991effed66fefdf134946b2f63d0edce
inzapp <keg9903@naver.com>
2019-08-13 11:29:22 +0900

    테스트이미지로 line detection

--- src/ArSudokuSolver.java
+++ src/ArSudokuSolver.java
@@ -14,21 +14,29 @@ public class ArSudokuSolver {
     public static void main(String[] args) {
         Mat raw = Imgcodecs.imread("sudoku.jpg", Imgcodecs.IMREAD_ANYCOLOR);
         Mat proc = raw.clone();
+
+        // pre processing
         Imgproc.cvtColor(proc, proc, Imgproc.COLOR_BGR2GRAY);
         Imgproc.adaptiveThreshold(proc, proc, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 201, 7);
         Core.bitwise_not(proc, proc);
+
+        // detect and draw line
         Mat lines = new Mat();
         Imgproc.HoughLines(proc, lines, 1, Math.PI / 180, 350);
         for (int x = 0; x < lines.rows(); x++) {
-            double rho = lines.get(x, 0)[0],
-                    theta = lines.get(x, 0)[1];
-            double a = Math.cos(theta), b = Math.sin(theta);
-            double x0 = a * rho, y0 = b * rho;
+            double rho = lines.get(x, 0)[0];
+            double theta = lines.get(x, 0)[1];
+            double a = Math.cos(theta);
+            double b = Math.sin(theta);
+            double x0 = a * rho;
+            double y0 = b * rho;
             Point pt1 = new Point(Math.round(x0 + 1000 * (-b)), Math.round(y0 + 1000 * (a)));
             Point pt2 = new Point(Math.round(x0 - 1000 * (-b)), Math.round(y0 - 1000 * (a)));
             Imgproc.line(raw, pt1, pt2, new Scalar(0, 255, 0), 2, Imgproc.LINE_AA, 0);
         }
-        System.out.println(lines.dump());
+
+        // detect and draw intersect position
+        
 
         HighGui.imshow("res", raw);
         HighGui.waitKey(0);
