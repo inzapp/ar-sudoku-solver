06bc22d184ac89b1bb8951896315f77f3f0e8e3b
inzapp <keg9903@naver.com>
2019-08-13 13:43:43 +0900

    pointRank클래스 생성, points의 edge 4개 검출 중

--- src/ArSudokuSolver.java
+++ src/ArSudokuSolver.java
@@ -3,12 +3,28 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ArSudokuSolver {
     static {
         System.load("C:\\inzapp\\lib\\opencv\\opencv_java410.dll");
+    }
+
+    static class PointRank {
+        PointRank(Point point) {
+            this.point = point;
+        }
+
+        void addRank(int rank) {
+            this.rank += rank;
+        }
+
+        int getRank() {
+            return this.rank;
+        }
+
+        int rank = 0;
+        Point point;
     }
 
     public static void main(String[] args) {
@@ -33,28 +49,56 @@ public class ArSudokuSolver {
             double y0 = b * rho;
             Point pt1 = new Point(Math.round(x0 + 1000 * (-b)), Math.round(y0 + 1000 * (a)));
             Point pt2 = new Point(Math.round(x0 - 1000 * (-b)), Math.round(y0 - 1000 * (a)));
-//            Imgproc.line(raw, pt1, pt2, new Scalar(0, 255, 0), 1, Imgproc.LINE_AA, 0);
+            // Imgproc.line(raw, pt1, pt2, new Scalar(0, 255, 0), 1, Imgproc.LINE_AA, 0);
         }
 
-        // detect sudoku rectangle
-        // 1. find contours
+        // detect sudoku contour
+        // find contours
         List<MatOfPoint> contourList = new ArrayList<>();
         Imgproc.findContours(proc, contourList, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-        for(int i=0; i<contourList.size(); ++i){
-//            Imgproc.drawContours(raw, contourList, i, new Scalar(0, 255, 0), 2);
-        }
 
-        // 2. convert contours to rect
-        List<Rect> rectList = new ArrayList<>();
-        MatOfPoint2f curPoly = new MatOfPoint2f();
+        // find biggest contour
+        double maxArea = 0;
+        Rect biggestRect = new Rect();
+        MatOfPoint biggestContour = new MatOfPoint();
         for (MatOfPoint contour : contourList) {
-            Imgproc.approxPolyDP(new MatOfPoint2f(contour.toArray()), curPoly, 1, true);
-            rectList.add(Imgproc.boundingRect(curPoly));
+            Rect rect = Imgproc.boundingRect(contour);
+            if (maxArea < rect.area()) {
+                biggestContour = contour;
+                biggestRect = rect;
+                maxArea = rect.area();
+            }
         }
+        Imgproc.drawContours(raw, Collections.singletonList(biggestContour), 0, new Scalar(0, 255, 0), 2);
+//        Imgproc.rectangle(raw, biggestRect, new Scalar(0, 255, 0), 2);
 
-        for(Rect cur : rectList) {
-            Imgproc.rectangle(raw, cur, new Scalar(0, 255, 0), 2);
-        }
+        // perspective transform with sudoku contour
+        // get 4 edge point of sudoku contour
+
+        // copy points to point rank and sort
+        Point[] points = biggestContour.toArray();
+        PointRank[] pointRanks = new PointRank[points.length];
+        for (int i = 0; i < pointRanks.length; i++)
+            pointRanks[i] = new PointRank(points[i]);
+
+        Arrays.sort(pointRanks, Comparator.comparingDouble(a -> a.point.x));
+        for (int i = 0; i < pointRanks.length; i++)
+            pointRanks[i].addRank(i + 1);
+
+        Arrays.sort(pointRanks, Comparator.comparingDouble(a -> a.point.y));
+        for (int i = 0; i < pointRanks.length; i++)
+            pointRanks[i].addRank(i + 1);
+
+        Arrays.sort(pointRanks, Comparator.comparingInt(a -> a.rank));
+
+        for (int i = 0; i < points.length; i++)
+            points[i] = pointRanks[i].point;
+
+        for (Point cur : points)
+            System.out.println(cur);
+
+        Imgproc.circle(raw, points[0], 10, new Scalar(0, 255, 0), 2);
+
 
         HighGui.imshow("res", raw);
         HighGui.waitKey(0);
