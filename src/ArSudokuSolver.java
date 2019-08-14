9b8dea3535615bbc621da754c9d2d7bb18d8030c
inzapp <keg9903@naver.com>
2019-08-14 11:22:38 +0900

    sudoku algorithm solver 구현중

--- src/ArSudokuSolver.java
+++ src/ArSudokuSolver.java
@@ -5,6 +5,58 @@ import org.opencv.imgproc.Imgproc;
 
 import java.util.*;
 
+class SudokuAlgorithmSolver {
+    private class Cell {
+        int val;
+        List<Integer> candidate;
+
+        Cell(int val) {
+            if (val == 0) {
+                this.val = 0;
+                candidate = new ArrayList<>(Arrays.asList(
+                        1, 2, 3, 4, 5, 6, 7, 8, 9
+                ));
+            } else {
+                this.val = val;
+                candidate = new ArrayList<>();
+            }
+        }
+
+        void remoteCandidate(int val) {
+            candidate.remove(new Integer(val));
+            if (candidate.size() == 1)
+                this.val = candidate.get(0);
+        }
+    }
+
+    public void solve() {
+        Scanner sc = new Scanner(System.in);
+        Cell[][][] pan = new Cell[9][3][3];
+        for (int sq = 0; sq < 9; ++sq) {
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    pan[sq][i][j] = new Cell(sc.nextInt());
+                }
+            }
+        }
+
+        pan[0][0][0].remoteCandidate(1);
+        pan[0][0][0].remoteCandidate(2);
+        pan[0][0][0].remoteCandidate(3);
+        pan[0][0][0].remoteCandidate(4);
+        pan[0][0][0].remoteCandidate(5);
+        pan[0][0][0].remoteCandidate(6);
+        pan[0][0][0].remoteCandidate(7);
+        System.out.println(pan[0][0][0].val);
+        pan[0][0][0].remoteCandidate(8);
+
+        for (int cur : pan[0][0][0].candidate)
+            System.out.print(cur + " ");
+
+        System.out.println(pan[0][0][0].val);
+    }
+}
+
 public class ArSudokuSolver {
     static {
         System.load("C:\\inzapp\\lib\\opencv\\opencv_java410.dll");
@@ -20,14 +72,38 @@ public class ArSudokuSolver {
     }
 
     public static void main(String[] args) {
+        new SudokuAlgorithmSolver().solve();
+        System.exit(0);
+
         // load image
-        Mat raw = Imgcodecs.imread("sudoku3.jpg", Imgcodecs.IMREAD_ANYCOLOR);
+        Mat raw = Imgcodecs.imread("C:\\inzapp\\sudoku\\s13.jpg", Imgcodecs.IMREAD_ANYCOLOR);
         Mat proc = raw.clone();
 
         // pre processing
         Imgproc.cvtColor(proc, proc, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.blur(proc, proc, new Size(2, 2));
         Imgproc.adaptiveThreshold(proc, proc, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 201, 7);
         Core.bitwise_not(proc, proc);
+
+        // detect line
+        Mat canny = proc.clone();
+        Imgproc.Canny(canny, canny, 100, 100);
+        Mat lines = new Mat();
+        Imgproc.HoughLines(canny, lines, 1, Math.PI / 180, 150);
+        for (int x = 0; x < lines.rows(); x++) {
+            double rho = lines.get(x, 0)[0];
+            double theta = lines.get(x, 0)[1];
+            double a = Math.cos(theta);
+            double b = Math.sin(theta);
+            double x0 = a * rho;
+            double y0 = b * rho;
+            Point pt1 = new Point(Math.round(x0 + 1000 * (-b)), Math.round(y0 + 1000 * (a)));
+            Point pt2 = new Point(Math.round(x0 - 1000 * (-b)), Math.round(y0 - 1000 * (a)));
+            Imgproc.line(raw, pt1, pt2, new Scalar(255, 0, 0), 2, Imgproc.LINE_AA, 0);
+        }
+
+        HighGui.imshow("img", raw);
+        HighGui.waitKey(0);
 
         // detect sudoku contour
         // find contours
@@ -48,7 +124,7 @@ public class ArSudokuSolver {
         }
         Imgproc.drawContours(raw, Collections.singletonList(biggestContour), 0, new Scalar(0, 255, 0), 2);
 //        Imgproc.rectangle(raw, biggestRect, new Scalar(0, 0, 255), 2);
-        for(Point cur : biggestContour.toArray())
+        for (Point cur : biggestContour.toArray())
             System.out.println(cur);
 
         // perspective transform with sudoku contour
@@ -113,23 +189,6 @@ public class ArSudokuSolver {
         Imgproc.circle(raw, topRight, 10, new Scalar(0, 0, 255), 2);
         Imgproc.circle(raw, bottomLeft, 10, new Scalar(0, 0, 255), 2);
         Imgproc.circle(raw, bottomRight, 10, new Scalar(0, 0, 255), 2);
-
-        // detect line
-        Mat canny = proc.clone();
-        Imgproc.Canny(canny, canny, 100, 100);
-        Mat lines = new Mat();
-        Imgproc.HoughLines(canny, lines, 1, Math.PI / 180, 150);
-        for (int x = 0; x < lines.rows(); x++) {
-            double rho = lines.get(x, 0)[0];
-            double theta = lines.get(x, 0)[1];
-            double a = Math.cos(theta);
-            double b = Math.sin(theta);
-            double x0 = a * rho;
-            double y0 = b * rho;
-            Point pt1 = new Point(Math.round(x0 + 1000 * (-b)), Math.round(y0 + 1000 * (a)));
-            Point pt2 = new Point(Math.round(x0 - 1000 * (-b)), Math.round(y0 - 1000 * (a)));
-//             Imgproc.line(raw, pt1, pt2, new Scalar(255, 0, 0), 2, Imgproc.LINE_AA, 0);
-        }
 
         HighGui.imshow("res", raw);
         HighGui.waitKey(0);
