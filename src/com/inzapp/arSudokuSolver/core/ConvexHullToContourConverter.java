056026cc6aac3cb4586adc52368df1c17224f0ac
inzapp <inzapp@naver.com>
2019-09-07 16:26:06 +0900

    sudoku array converter refactor

---
+++ src/com/inzapp/arSudokuSolver/core/ConvexHullToContourConverter.java
@@ -0,0 +1,47 @@
+package com.inzapp.arSudokuSolver.core;
+
+import com.inzapp.arSudokuSolver.config.Config;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvexHullToContourConverter {
+    /**
+     *
+     * @param progress
+     * @param sudokuContour
+     * @return
+     */
+    public MatOfPoint convert(Mat progress, MatOfPoint sudokuContour) {
+        if(!Config.VIEW_PROGRESS)
+            return null;
+
+        try {
+            // convex hull
+            MatOfInt hull = new MatOfInt();
+            Imgproc.convexHull(sudokuContour, hull);
+
+            // get hull idx list
+            Point[] contourArray = sudokuContour.toArray();
+            Point[] hullPoints = new Point[hull.rows()];
+
+            // copy hull point to array
+            List<Integer> hullContourIdxList = hull.toList();
+            for (int i = 0; i < hullContourIdxList.size(); i++)
+                hullPoints[i] = contourArray[hullContourIdxList.get(i)];
+
+            // convert hull to list
+            List<MatOfPoint> hullList = new ArrayList<>();
+            hullList.add(new MatOfPoint(hullPoints));
+
+            // draw hull
+            Imgproc.drawContours(progress, hullList, 0, new Scalar(0, 255, 0), 2);
+
+            return new MatOfPoint(hullPoints);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
---
+++ src/com/inzapp/arSudokuSolver/core/SudokuArrayConverter.java
@@ -0,0 +1,85 @@
+package com.inzapp.arSudokuSolver.core;
+
+import com.inzapp.arSudokuSolver.config.Config;
+import com.inzapp.arSudokuSolver.util.View;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.ml.ANN_MLP;
+import org.opencv.ml.StatModel;
+
+public class SudokuArrayConverter {
+    private StatModel model;
+
+    public SudokuArrayConverter() {
+        model = ANN_MLP.load("model.xml");
+    }
+
+    public int[][] convert(Mat perspectiveTransformer, Mat proc) {
+        // perspective transform with processing sudoku contour
+        Imgproc.warpPerspective(proc, proc, perspectiveTransformer, proc.size());
+
+        Mat[][] elements = this.split(proc, 28);
+        Mat res = new Mat();
+        int[][] unsolvedSudoku = new int[9][9];
+        for (int i = 0; i < elements.length; ++i) {
+            for (int j = 0; j < elements[i].length; ++j) {
+                Mat cur = elements[i][j];
+                cur.convertTo(cur, CvType.CV_32FC1, 1 / 255.0f);
+                cur = cur.reshape(cur.channels(), 1);
+                unsolvedSudoku[i][j] = (int) model.predict(cur, res);
+            }
+        }
+
+        // print unsolved sudoku
+        if (Config.VIEW_PROGRESS) {
+            for (int[] row : unsolvedSudoku) {
+                for (int cur : row)
+                    System.out.print(cur + " ");
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        return unsolvedSudoku;
+    }
+
+    public Mat[] getPerspectiveTransformers(Point[] corners, Mat proc) {
+        // calculate perspective transformer
+        Mat before = new MatOfPoint2f(corners[0], corners[1], corners[2], corners[3]);
+        Mat after = new MatOfPoint2f(new Point(0, 0), new Point(proc.cols(), 0),
+                new Point(0, proc.rows()), new Point(proc.cols(), proc.rows()));
+        Mat perspectiveTransformer = Imgproc.getPerspectiveTransform(before, after);
+        Mat inverseTransformer = Imgproc.getPerspectiveTransform(after, before);
+        return new Mat[]{perspectiveTransformer, inverseTransformer};
+    }
+
+    private Mat[][] split(Mat proc, int splitSize) {
+        // resize to (28 * 9) * (28 * 9) : 28 is column of train data
+        if (Config.VIEW_PROGRESS) {
+            Mat transform = new Mat();
+            Imgproc.resize(proc, transform, new Size(splitSize * 15, splitSize * 15));
+            View.transform = transform;
+        }
+
+        Imgproc.resize(proc, proc, new Size(splitSize * 9, splitSize * 9));
+        // split mat into 9 * 9
+        Mat[][] elements = new Mat[9][9];
+        int offset = splitSize;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j)
+                elements[i][j] = proc.
+                        rowRange(i * offset, i * offset + offset).
+                        colRange(j * offset, j * offset + offset);
+        }
+
+        if (Config.VIEW_PROGRESS) {
+            for (int i = 0; i < elements.length; ++i) {
+                for (int j = 0; j < elements[i].length; ++j) {
+                    Mat elementView = new Mat();
+                    Imgproc.resize(elements[i][j], View.elements[i][j], new Size(splitSize * 15 / 9, splitSize * 15 / 9));
+                }
+            }
+        }
+        return elements;
+    }
+}
